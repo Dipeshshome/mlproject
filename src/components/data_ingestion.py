46eea8a2cd5c03966f76ec3232c60e11d55ceeab
Dipesh Shome <C:/Users/DIPESH SHOME>
2023-07-31 10:59:48 +0600

    Data Ingestion updated

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -20,7 +20,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the ingestion method or component")    
         try:
-            df=pd.read_csv('notebook\data\stud.csv')
+            df=pd.read_csv('notebook/data/stud.csv')
             logging.info('Exported the dataset')
             
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -28,8 +28,8 @@ class DataIngestion:
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
             logging.info("Train test split initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
-            train_set.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
-            test_set.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
             logging.info("Ingestion completed")
 
@@ -43,4 +43,6 @@ class DataIngestion:
             raise CustomException(e,sys)
 
 
- 
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,6 @@
 import sys 
 from src.logger import logging
 
-
 def error_message_detail(error,error_details:sys):
     _,_,exc_tb=error_details.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
@@ -15,6 +14,7 @@ class CustomException(Exception):
         self.error_message=error_message_detail(error_message,error_details=error_details)
 
     def __str__(self):
+        logging.info('Exception')
         return self.error_message        
 
 # if __name__=="__main__":
